93be0bb1e35437e21b9bb70aaa02bd177fb3ba33
MustafaKoceerr <128032366+MustafaKoceerr@users.noreply.github.com>
2024-07-19 21:30:14 +0300

    Inheritance.kt and Polymorphism.kt added

---
+++ src/main/kotlin/classes/Polymorphism.kt
@@ -0,0 +1,79 @@
+package org.example.classes
+
+/**
+ *  Ust class'ta bir fonksiyon var, child class'lardaki fonksiyonlar çıktıyı değiştiriyorsa
+ *  buna polymorphism denir.
+ *
+ * Direkt olarak open yaptığımız bir property'yi (değişken veya fonksiyon) override
+ * etmesine Polymorphism denir ama->
+ * DYNAMIC POLYMORPHISM denir.
+ * override ile dinamik olarak fonksiyonu değiştiriyorum bu yüzden dynamic polymorphism
+ * makeASound dog için farklı, cat için farklı
+ */
+open class Animal2(val name: String, open val species: String) {
+    open var footCount = 4
+    /**
+     * Not: Bir parametreyi open yapmayacaksan, basitçe val yaparsın ve set'ini private yapmış olursun
+     * var yaparsan, open yapmasan bile tekrar atanabilir olacaktır. Bu da anlamlı olmaz.
+     * Open yaptığın bir şeyin alt class'lar tarafından atanabilir olması gerekir.
+     * Open yapmadan var değişkene alt sınıflardan değer atama
+     */
+    val tailShape = "YUVARLAK"
+
+    open fun makeASound(): String {
+        return ""
+    }
+}
+
+class Dog2(name: String, override val species: String, footCount: Int) : Animal2(name, species) {
+
+    override var footCount: Int = footCount
+        set(value) {
+            field = value
+        }
+
+    override fun makeASound(): String {
+        return "HavHavHav:"
+    }
+
+    fun call(gokhan:Gokhan){
+        gokhan.foo()
+    }
+
+    /**
+     * Ayni class icinde 2 tane ayni isimli fonksiyon ama baska isler yapiyorlar,
+     * buna polymorphism denir ama
+     * STATIC POLYMORPHISM denir.
+     * alacakları değerler statik oluyor,fonksiyonun alacağı değerler belli, geri döndürecekleri sonuçlar belli
+     * bu sabit bir bilgi olduğu için static polymorphism oluyor.
+     */
+    fun call(musti:Musti){
+        musti.boo()
+    }
+}
+
+class Gokhan{
+    fun foo(){
+        println("Gokhan foo")
+    }
+}
+class Musti{
+    fun boo(){
+        println("Musti boo")
+    }
+}
+
+fun main() {
+    val dog = Dog2("Karabas","Dog",4)
+
+//    dog.tailShape = "HEYHEY"
+
+    println("Dog name: ${dog.name}")
+    println("Dog species: ${dog.species}")
+    println("Dog footCount: ${dog.footCount}")
+    println("Dog tailShape: ${dog.tailShape}")
+
+    println("Dog: ${dog.toString()}")
+    println("Dog makeASound: ${dog.makeASound()}")
+
+}
