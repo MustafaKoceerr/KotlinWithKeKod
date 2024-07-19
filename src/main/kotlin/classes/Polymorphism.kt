6168f6bb8e87e4063b89f782a09ff6e3218b2d26
MustafaKoceerr <128032366+MustafaKoceerr@users.noreply.github.com>
2024-07-19 21:37:28 +0300

    polymorphism updated

--- src/main/kotlin/classes/Polymorphism.kt
+++ src/main/kotlin/classes/Polymorphism.kt
@@ -12,6 +12,7 @@ package org.example.classes
  */
 open class Animal2(val name: String, open val species: String) {
     open var footCount = 4
+
     /**
      * Not: Bir parametreyi open yapmayacaksan, basitçe val yaparsın ve set'ini private yapmış olursun
      * var yaparsan, open yapmasan bile tekrar atanabilir olacaktır. Bu da anlamlı olmaz.
@@ -36,7 +37,7 @@ class Dog2(name: String, override val species: String, footCount: Int) : Animal2
         return "HavHavHav:"
     }
 
-    fun call(gokhan:Gokhan){
+    fun call(gokhan: Gokhan) {
         gokhan.foo()
     }
 
@@ -44,27 +45,51 @@ class Dog2(name: String, override val species: String, footCount: Int) : Animal2
      * Ayni class icinde 2 tane ayni isimli fonksiyon ama baska isler yapiyorlar,
      * buna polymorphism denir ama
      * STATIC POLYMORPHISM denir.
+     * Static polymorphism'de override yok.
      * alacakları değerler statik oluyor,fonksiyonun alacağı değerler belli, geri döndürecekleri sonuçlar belli
      * bu sabit bir bilgi olduğu için static polymorphism oluyor.
      */
-    fun call(musti:Musti){
+    fun call(musti: Musti) {
         musti.boo()
+    }
+
+
+    // bunu şöyle de yapabilirdin.
+    fun call(male: Male) {
+        if (male is Gokhan) {
+            (male as Gokhan).foo()
+        } else {
+            (male as Musti).boo()
+        }
+    }
+
+    // bunu şöyle de yapabilirdin.
+    fun call(gokhan: Gokhan? = null, musti: Musti? = null) {
+        if (gokhan==null){
+            musti?.boo()
+        }else{
+            gokhan.foo()
+        }
     }
 }
 
-class Gokhan{
-    fun foo(){
+class Gokhan : Male() {
+    fun foo() {
         println("Gokhan foo")
     }
 }
-class Musti{
-    fun boo(){
+
+class Musti : Male() {
+    fun boo() {
         println("Musti boo")
     }
 }
 
+open class Male()
+
+
 fun main() {
-    val dog = Dog2("Karabas","Dog",4)
+    val dog = Dog2("Karabas", "Dog", 4)
 
 //    dog.tailShape = "HEYHEY"
 
